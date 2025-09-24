a10d91ec86f640f38aa78a3d483cf8530fe841d8
christianzaboli <zabolichristian@gmail.com>
2025-09-24 20:38:00 +0200

    barra links fixata con le immagini

--- src/components/MyLinks.jsx
+++ src/components/MyLinks.jsx
@@ -1,4 +1,9 @@
 // creo ed esporto component
+import ShopComics from "../assets/buy-comics-digital-comics.png"
+import ShopMerchandise from "../assets/buy-comics-merchandise.png"
+import ShopSubscription from "../assets/buy-comics-subscriptions.png"
+import ShopLocator from "../assets/buy-comics-shop-locator.png"
+import PowerVisa from "../assets/buy-dc-power-visa.svg"
 
 const MyLinks = () => {
     return (
@@ -7,39 +12,40 @@ const MyLinks = () => {
 
                 <a href="#">
                     <div className="single-link">
-                        <img src="../assets/buy-comics-digital-comics.png" alt="Shop Comincs" />
+                        <img src={ShopComics}
+                        alt="Shop Comics" />
                         <h3>DIGITAL COMICS</h3>
                     </div>
                 </a>
 
                 <a href="#">
                     <div className="single-link">
-                        <img src="../assets/buy-comics-merchandise.png" alt="Shop Merchandise" />
+                        <img src={ShopMerchandise} alt="Shop Merchandise" />
                         <h3>DC MERCHANDISE</h3>
                     </div>
                 </a>
 
                 <a href="#">
                     <div className="single-link">
-                        <img src="../assets/buy-comics-subscriptions.png" alt="Shop Subscription" />
+                        <img src={ShopSubscription} alt="Shop Subscription" />
                         <h3>SUBSCRIPTION</h3>
                     </div>
                 </a>
 
                 <a href="#">
                     <div className="single-link">
-                        <img src="../assets/buy-comics-shop-locator.png" alt="Shop Lovator" />
+                        <img src={ShopLocator} alt="Shop Locator" />
                         <h3>COMIC SHOP LOCATOR</h3>
                     </div>
                 </a>
 
                 <a href="#">
                     <div className="single-link">
-                        <img src="../assets/buy-dc-power-visa.svg" alt="Power Visa" />
+                        <img src={PowerVisa} alt="Power Visa" />
                         <h3>DC POWER VISA</h3>
                     </div>
                 </a>
-                
+
             </div>
 
         </div>
